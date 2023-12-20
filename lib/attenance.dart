8ae5e06dadc6f4c9a68a1e45e7f411a597dd42ec
TBIPC <yousifTBI>
2023-12-20 12:23:57 +0200

    updatedg

--- lib/attenance.dart
+++ lib/attenance.dart
@@ -1,6 +1,11 @@
+import 'package:attendance/LocationModelApi.dart';
+import 'package:attendance/task.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class attenance extends StatelessWidget {
   double? latitude;
@@ -56,8 +61,11 @@ class attenance extends StatelessWidget {
           // Check the label to determine which action to perform
           print('Button pressed: $label');
           if (label == 'SUBMIT') {
+
             getLocation();
             getCurrentLocation();
+            // LocationModelApi locationData = LocationModelApi(longitude!, latitude!,  158);
+            // getAttendanceState(locationData);
           }
         },
         icon: Icon(
@@ -82,7 +90,9 @@ class attenance extends StatelessWidget {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
-     // print("Latitude: ${position.latitude}, Longitude: ${position.longitude}");
+      print("Latitude: ${position.latitude}, Longitude: ${position.longitude}");
+      LocationModelApi locationData = LocationModelApi(position.longitude!, position.latitude!,  158);
+      getAttendanceState(locationData);
     } catch (e) {
       print("Error getting location: $e");
     }
@@ -102,4 +112,39 @@ class attenance extends StatelessWidget {
     }
   }
 
+  Future<void> getAttendanceState(LocationModelApi locationData) async {
+    final String baseUrl = "https://attendance-api.tbico.cloud/";
+    final String endpoint = "api/Employee/EmployeeAttendanceV2";
+
+    try {
+      final Uri uri = Uri.parse('$baseUrl$endpoint');
+
+      // Convert the LocationModelApi object to a map
+      final Map<String, dynamic> locationMap = locationData.toMap();
+
+      final response = await http.post(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        // Convert the location data to JSON and include it in the request body
+        body: jsonEncode(locationMap),
+      );
+
+      if (response.statusCode == 200) {
+        // If the server returns a 200 OK response, parse the JSON
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        // task taskvar = task();
+        // taskvar.message;
+        print('Attendance State: $data');
+
+      } else {
+        // If the server did not return a 200 OK response,
+        // throw an exception.
+        print('Failed to get attendance state. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
 }
